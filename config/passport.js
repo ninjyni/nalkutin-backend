80cc958c138f323503c1c3f79aaa7a94908775cf
Nina Tyni <nina.tyni@gmail.com>
2018-08-12 21:24:05 +0300

    Use passport strategy for username/password login

--- config/passport.js
+++ config/passport.js
@@ -1,8 +1,9 @@
-var config = require('../config/config');
-var JwtStrategy = require('passport-jwt').Strategy,
-    ExtractJwt = require('passport-jwt').ExtractJwt;
-var models  = require('../models');
-var User = models.user;
+var config = require('../config/config'),
+    JwtStrategy = require('passport-jwt').Strategy,
+    LocalStrategy = require('passport-local').Strategy,
+    ExtractJwt = require('passport-jwt').ExtractJwt,
+    models  = require('../models'),
+    User = models.user;
 
 module.exports = function(passport) {
   var opts = {}
@@ -18,6 +19,23 @@ module.exports = function(passport) {
       return token;
     }
   ]);
+
+  passport.use(new LocalStrategy(function(username, password, done) {
+      User.findOne({
+        where: {
+          username: username
+        }
+      }).then(function(user) {
+        if (user && user.validPassword(password)) {
+          return done(null, user);
+        } else {
+          return done(null, false, {message: 'Incorrect username or password.'});
+        }
+      }).catch(function(error) {
+        return done(error);
+      });
+    }
+  ));
 
   passport.use(new JwtStrategy(opts, function(jwt_payload, done) {
     User.findOne({
--- routes/login.js
+++ routes/login.js
@@ -1,46 +1,38 @@
-var models  = require('../models');
-var config = require('../config/config');
-var express = require('express');
-var passport = require('passport');
-var jwt = require('jsonwebtoken');
-var router = express.Router();
-var User = models.user;
+var models  = require('../models'),
+    config = require('../config/config'),
+    express = require('express'),
+    passport = require('passport'),
+    jwt = require('jsonwebtoken'),
+    router = express.Router(),
+    User = models.user;
 
-router.route('/')
-.post(function(req, res) {
-  User.findOne({
-    where: {
-      username: req.body.username
+router.post('/', function (req, res) {
+  passport.authenticate('local', {session: false}, (error, user, message) => {
+    if (error || !user) {
+      return res.status(401).json(message);
     }
-  }).then(function(user) {
-    if (!user) {
-      res.sendStatus(401);
-    } else {
-      if (user.validPassword(req.body.password)) {
-        var payload = {
-          id: user.id,
-          expires: Date.now() + parseInt(process.env.JWT_EXPIRATION_MS || 3600000)
-        };
-        var token = jwt.sign(payload, process.env.JWT_SECRET || config.secret);
 
-        res.format({
-          json: function() {
-            res.json({success: true, token: 'Bearer ' + token});
-          },
-          html: function() {
-            res.cookie('token', token, {
-              httpOnly: true,
-              maxAge: 3600000
-            }).redirect('back');
-          }
-        });
-      } else {
-        res.sendStatus(401);
+    var payload = {
+      id: user.id,
+      expires: Date.now() + parseInt(process.env.JWT_EXPIRATION_MS || 3600000)
+    };
+    var token = jwt.sign(payload, process.env.JWT_SECRET || config.secret);
+
+    res.format({
+      json: function() {
+        res.json({success: true, token: 'Bearer ' + token});
+      },
+      html: function() {
+        res.cookie('token', token, {
+          httpOnly: true,
+          maxAge: 3600000
+        }).redirect('back');
       }
-    }
-  });
-})
-.delete(function(req, res) {
+    });
+  }) (req, res);
+});
+
+router.delete('/', function (req, res) {
   res.clearCookie('token');
   res.sendStatus(200);
 });
